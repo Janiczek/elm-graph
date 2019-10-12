143fc9df586116148f212173e75db3fa70a00969
Martin Janiczek <martin@janiczek.cz>
2019-10-12 20:17:19 +0200

    Initial

---
+++ src/Graph.elm
@@ -0,0 +1,315 @@
+module Graph exposing
+    ( Graph, empty
+    , addEdge, removeEdge, addVertex, removeVertex
+    , hasEdge, hasVertex, areAdjacent
+    , vertices, edges, outgoingEdges, neighbours
+    )
+
+{-|
+
+
+# Construction
+
+@docs Graph, empty
+
+
+# Modification
+
+@docs addEdge, removeEdge, addVertex, removeVertex
+
+
+# Predicates
+
+@docs hasEdge, hasVertex, areAdjacent
+
+
+# Querying
+
+@docs vertices, edges, outgoingEdges, neighbours
+
+  - TODO some helper function for getting ID of vertex, and getting vertex of ID
+  - TODO undirected variant, simpler API
+  - TODO module documentation
+  - TODO think about metadata for vertices. Is the type parameter enough? Is it
+    OK that the user would have to add their own IDs or whatever?
+  - TODO for now we don't have any metadata for edges, but think about adding that!
+      - getEdgeValue : vertex -> vertex -> Graph vertex edge -> Maybe edge
+      - setEdgeValue : vertex -> vertex -> edge -> Graph vertex edge -> Maybe edge
+      - would it have to be mandatory? or optional and wrapped in Maybe?
+        -- TODO fromList? That's the kind of API I don't like much about the elm-community lib...
+  - TODO currently user can't have two vertices with same data... that's probably OK, right?
+
+-}
+
+import AssocList as Dict exposing (Dict)
+import Maybe.Extra
+import Set exposing (Set)
+
+
+type Graph vertex
+    = Graph
+        { -- TODO maybe use elm-community/intdict too
+          edges : Dict Int (Set Int)
+        , -- TODO can we do without the reverse index here?
+          vertices : Dict vertex Int
+        , verticesById : Dict Int vertex
+        , unusedId : Int
+        }
+
+
+{-| An empty graph.
+-}
+empty : Graph vertex
+empty =
+    Graph
+        { edges = Dict.empty
+        , vertices = Dict.empty
+        , verticesById = Dict.empty
+        , unusedId = 0
+        }
+
+
+hasVertex : vertex -> Graph vertex -> Bool
+hasVertex vertex (Graph g) =
+    Dict.member vertex g.vertices
+
+
+{-| Directed variant of `areAdjacent`
+-}
+hasEdge : vertex -> vertex -> Graph vertex -> Bool
+hasEdge from to (Graph g) =
+    let
+        maybeEdgesFrom =
+            Dict.get from g.vertices
+                |> Maybe.andThen (\fromId -> Dict.get fromId g.edges)
+
+        maybeToId =
+            Dict.get to g.vertices
+    in
+    Maybe.map2 Set.member
+        maybeToId
+        maybeEdgesFrom
+        |> Maybe.withDefault False
+
+
+{-| Does the graph contain an edge between these two vertices?
+
+Undirected - the order of the two vertices doesn't matter.
+
+-}
+areAdjacent : vertex -> vertex -> Graph vertex -> Bool
+areAdjacent v1 v2 graph =
+    hasEdge v1 v2 graph || hasEdge v2 v1 graph
+
+
+{-| Lists all vertices which to which the given vertex "points".
+
+Directed - the edge can only go _from_ the given vertex.
+
+     Graph.empty
+         |> Graph.outgoingEdges "foo"
+     -->
+     []
+
+     Graph.empty
+         |> Graph.addEdge "foo" "bar"
+         |> Graph.outgoingEdges "foo"
+     -->
+     [ "bar" ]
+
+TODO rethink naming?
+
+-}
+outgoingEdges : vertex -> Graph vertex -> List vertex
+outgoingEdges vertex (Graph g) =
+    Dict.get vertex g.vertices
+        |> Maybe.andThen (\vertexId -> Dict.get vertexId g.edges)
+        |> Maybe.map Set.toList
+        |> Maybe.andThen
+            (List.map (\toId -> Dict.get toId g.verticesById)
+                >> Maybe.Extra.combine
+            )
+        |> Maybe.withDefault []
+
+
+{-| Lists all vertices which share an edge with the given vertex.
+
+Undirected - the edge can go _from_ the given vertex or _to_ the given vertex.
+
+TODO do we want this? what to hold in memory to make this easier?
+
+-}
+neighbours : vertex -> Graph vertex -> List vertex
+neighbours vertex (Graph g) =
+    Debug.todo "neighbours"
+
+
+{-| Add the vertex to the graph. (Nothing happens if it's already present.)
+
+By default it's not connected to any other vertex.
+
+-}
+addVertex : vertex -> Graph vertex -> Graph vertex
+addVertex vertex ((Graph g) as graph) =
+    if hasVertex vertex graph then
+        graph
+
+    else
+        Graph
+            { g
+                | vertices = Dict.insert vertex g.unusedId g.vertices
+                , verticesById = Dict.insert g.unusedId vertex g.verticesById
+                , unusedId = g.unusedId + 1
+            }
+
+
+{-| Remove the vertex from the graph. (Nothing happens if it's not present.)
+-}
+removeVertex : vertex -> Graph vertex -> Graph vertex
+removeVertex vertex ((Graph g) as graph) =
+    Dict.get vertex g.vertices
+        |> Maybe.map
+            (\id ->
+                Graph
+                    { g
+                        | edges =
+                            g.edges
+                                -- outgoing
+                                |> Dict.filter (\k v -> k /= id)
+                                -- incoming
+                                |> Dict.map
+                                    (\k v ->
+                                        -- TODO maybe do member check first?
+                                        Set.remove id v
+                                    )
+                        , vertices = Dict.remove vertex g.vertices
+                        , verticesById = Dict.remove id g.verticesById
+                    }
+            )
+        |> Maybe.withDefault graph
+
+
+{-| Add an edge from the first vertex to the second one.
+
+If any of the vertices aren't present yet, this function **will add them** before
+adding the edge.
+
+Directed - the order of the vertices matters!
+
+TODO example
+TODO think about {from: vertex, to: vertex} API
+TODO think about (vertex, vertex) API
+
+-}
+addEdge : vertex -> vertex -> Graph vertex -> Graph vertex
+addEdge from to graph =
+    graph
+        |> addVertex from
+        |> addVertex to
+        |> addEdge_ from to
+
+
+{-| A helper for `addEdge` that doesn't check if the vertices are present.
+-}
+addEdge_ : vertex -> vertex -> Graph vertex -> Graph vertex
+addEdge_ from to ((Graph g) as graph) =
+    if hasEdge from to graph then
+        graph
+
+    else
+        let
+            maybeFromId =
+                Dict.get from g.vertices
+
+            maybeToId =
+                Dict.get to g.vertices
+        in
+        Maybe.map2
+            (\fromId toId ->
+                Graph
+                    { g
+                        | edges =
+                            Dict.update fromId
+                                (\maybeEdgesFrom ->
+                                    case maybeEdgesFrom of
+                                        Nothing ->
+                                            Just (Set.singleton toId)
+
+                                        Just edgesFrom ->
+                                            Just (Set.insert toId edgesFrom)
+                                )
+                                g.edges
+                    }
+            )
+            maybeFromId
+            maybeToId
+            |> Maybe.withDefault graph
+
+
+{-| Remove an edge from the first vertex to the second one.
+
+If the edge is not present, nothing happens.
+
+Directed - the order of the vertices matters!
+
+TODO example
+TODO think about {from: vertex, to: vertex} API
+TODO think about (vertex, vertex) API
+
+-}
+removeEdge : vertex -> vertex -> Graph vertex -> Graph vertex
+removeEdge from to ((Graph g) as graph) =
+    Maybe.map2
+        (\fromId toId ->
+            Graph
+                { g
+                    | edges =
+                        Dict.update fromId
+                            (\maybeTos ->
+                                case maybeTos of
+                                    Nothing ->
+                                        Nothing
+
+                                    Just tos ->
+                                        Just (Set.remove toId tos)
+                            )
+                            g.edges
+                }
+        )
+        (Dict.get from g.vertices)
+        (Dict.get to g.vertices)
+        |> Maybe.withDefault graph
+
+
+{-| TODO
+-}
+vertices : Graph vertex -> List vertex
+vertices (Graph g) =
+    Dict.keys g.vertices
+
+
+{-| TODO
+-}
+edges : Graph vertex -> List { from : vertex, to : vertex }
+edges (Graph g) =
+    g.edges
+        |> Dict.toList
+        |> List.concatMap
+            (\( from, tos ) ->
+                tos
+                    |> Set.toList
+                    |> List.map (\to -> ( from, to ))
+            )
+        |> List.map
+            (\( fromId, toId ) ->
+                Maybe.map2
+                    (\from to ->
+                        { from = from
+                        , to = to
+                        }
+                    )
+                    (Dict.get fromId g.verticesById)
+                    (Dict.get toId g.verticesById)
+            )
+        |> Maybe.Extra.combine
+        |> Maybe.withDefault []
---
+++ tests/GraphTest.elm
@@ -0,0 +1,252 @@
+module GraphTest exposing (..)
+
+import ArchitectureTest exposing (msgTest)
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer)
+import Graph exposing (Graph)
+import Test exposing (Test, describe, test, todo)
+
+
+
+-- EXAMPLES
+
+
+graphWithFoo : Graph String
+graphWithFoo =
+    Graph.empty
+        |> Graph.addVertex "foo"
+
+
+graphWithFooBar : Graph String
+graphWithFooBar =
+    Graph.empty
+        |> Graph.addVertex "foo"
+        |> Graph.addVertex "bar"
+
+
+graphWithFooBarWithEdge : Graph String
+graphWithFooBarWithEdge =
+    Graph.empty
+        |> Graph.addEdge "foo" "bar"
+
+
+graphWithFooBarWithReverseEdge : Graph String
+graphWithFooBarWithReverseEdge =
+    Graph.empty
+        |> Graph.addEdge "bar" "foo"
+
+
+
+-- ARCHITECTURE TEST STUFF
+
+
+type alias Model =
+    Graph String
+
+
+type Msg
+    = AddEdge String String
+    | RemoveEdge String String
+    | AddVertex String
+    | RemoveVertex String
+
+
+app : ArchitectureTest.TestedApp Model Msg
+app =
+    { model =
+        ArchitectureTest.OneOfModels
+            [ Graph.empty
+            , graphWithFoo
+            , graphWithFooBar
+            , graphWithFooBarWithEdge
+            , graphWithFooBarWithReverseEdge
+            ]
+    , update = ArchitectureTest.UpdateWithoutCmds update
+    , msgFuzzer = msgFuzzer
+    , msgToString = msgToString
+    , modelToString = modelToString
+    }
+
+
+update : Msg -> Model -> Model
+update msg graph =
+    case msg of
+        AddEdge from to ->
+            Graph.addEdge from to graph
+
+        RemoveEdge from to ->
+            Graph.removeEdge from to graph
+
+        AddVertex vertex ->
+            Graph.addVertex vertex graph
+
+        RemoveVertex vertex ->
+            Graph.removeVertex vertex graph
+
+
+msgToString : Msg -> String
+msgToString msg =
+    case msg of
+        AddEdge from to ->
+            "AddEdge " ++ Debug.toString from ++ " " ++ Debug.toString to
+
+        RemoveEdge from to ->
+            "RemoveEdge " ++ Debug.toString from ++ " " ++ Debug.toString to
+
+        AddVertex vertex ->
+            "AddVertex " ++ Debug.toString vertex
+
+        RemoveVertex vertex ->
+            "RemoveVertex " ++ Debug.toString vertex
+
+
+modelToString : Model -> String
+modelToString graph =
+    let
+        vertices =
+            Graph.vertices graph
+
+        edges =
+            Graph.edges graph
+    in
+    "<GRAPH vertices="
+        ++ Debug.toString vertices
+        ++ " edges="
+        ++ Debug.toString edges
+        ++ ">"
+
+
+vertexFuzzer : Fuzzer String
+vertexFuzzer =
+    Fuzz.oneOf
+        [ Fuzz.constant "foo"
+        , Fuzz.constant "bar"
+        , Fuzz.constant "baz"
+        , Fuzz.constant "quux"
+        , Fuzz.string
+        ]
+
+
+addEdgeFuzzer : Fuzzer Msg
+addEdgeFuzzer =
+    Fuzz.map2 AddEdge vertexFuzzer vertexFuzzer
+
+
+removeEdgeFuzzer : Fuzzer Msg
+removeEdgeFuzzer =
+    Fuzz.map2 RemoveEdge vertexFuzzer vertexFuzzer
+
+
+addVertexFuzzer : Fuzzer Msg
+addVertexFuzzer =
+    Fuzz.map AddVertex vertexFuzzer
+
+
+removeVertexFuzzer : Fuzzer Msg
+removeVertexFuzzer =
+    Fuzz.map RemoveVertex vertexFuzzer
+
+
+msgFuzzer : Fuzzer Msg
+msgFuzzer =
+    Fuzz.oneOf
+        [ addEdgeFuzzer
+        , removeEdgeFuzzer
+        , addVertexFuzzer
+        , removeVertexFuzzer
+        ]
+
+
+
+-- TESTS
+
+
+suite : Test
+suite =
+    describe "Graph"
+        [ describe "hasVertex"
+            [ test "returns True if the vertex is present" <|
+                \() ->
+                    Graph.hasVertex "foo" graphWithFoo
+                        |> Expect.true ""
+            , test "returns False if the vertex is not present" <|
+                \() ->
+                    Graph.hasVertex "bar" graphWithFoo
+                        |> Expect.false ""
+            ]
+        , describe "hasEdge"
+            [ test "returns True if the edge is present" <|
+                \() ->
+                    Graph.hasEdge "foo" "bar" graphWithFooBarWithEdge
+                        |> Expect.true ""
+            , test "returns False if the `from` vertex is not present" <|
+                \() ->
+                    Graph.hasEdge "foo" "bar" Graph.empty
+                        |> Expect.false ""
+            , test "returns False if the `to` vertex is not present" <|
+                \() ->
+                    Graph.hasEdge "foo" "baz" graphWithFooBarWithEdge
+                        |> Expect.false ""
+            , test "returns False if the edge is not present" <|
+                \() ->
+                    Graph.hasEdge "foo" "bar" graphWithFooBar
+                        |> Expect.false ""
+            , test "returns False if the edge is in wrong direction" <|
+                \() ->
+                    Graph.hasEdge "foo" "bar" graphWithFooBarWithReverseEdge
+                        |> Expect.false ""
+            ]
+        , describe "addEdge"
+            [ todo "adds `from` vertex if it isn't present"
+            , todo "adds `to` vertex if it isn't present"
+            , todo "fuzz: if addEdge, then hasEdge = True"
+            , todo "fuzz: is a no-op if the edge already exists"
+            ]
+        , describe "areAdjacent"
+            [ todo "architecture msg test: order doesn't matter"
+            , todo "works when the `from` vertex has single edge"
+            , todo "works when the `from` vertex has multiple edges"
+            , todo "returns False when the `from` vertex doesn't have any edges"
+            , test "returns False when the `from` vertex isn't present" <|
+                \() ->
+                    Graph.areAdjacent "bar" "baz" graphWithFoo
+                        |> Expect.false ""
+            ]
+        , describe "outgoingEdges"
+            [ test "returns empty list if vertex isn't present" <|
+                \() ->
+                    Graph.outgoingEdges "foo" Graph.empty
+                        |> Expect.equalLists []
+            , test "returns empty list if vertex has no edges" <|
+                \() ->
+                    Graph.outgoingEdges "foo" graphWithFoo
+                        |> Expect.equalLists []
+            , test "returns a list of edges if vertex has them" <|
+                \() ->
+                    Graph.outgoingEdges "foo" graphWithFooBarWithEdge
+                        |> Expect.equalLists [ "bar" ]
+            , todo "is directed"
+            ]
+        , describe "removeVertex"
+            [ msgTest "removes all outgoing edges"
+                app
+                removeVertexFuzzer
+              <|
+                \_ msg finalGraph ->
+                    let
+                        removedVertex =
+                            case msg of
+                                RemoveVertex vertex ->
+                                    vertex
+
+                                _ ->
+                                    -- shouldn't happen
+                                    ""
+                    in
+                    Graph.vertices finalGraph
+                        |> List.all (\otherVertex -> not <| Graph.hasEdge removedVertex otherVertex finalGraph)
+                        |> Expect.true ""
+            , todo "removes all incoming edges"
+            , todo "architecture msg test: removeVertex v -> hasVertex v == False"
+            ]
+        ]
